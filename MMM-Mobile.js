5fc16d47a3af1989dd1ff6cccb37f7ac288cdfec
slametps <slametps@gmail.com>
2017-03-19 19:00:47 +0700

    added Indonesian translation
    added Indonesian translation

--- MMM-Mobile.js
+++ MMM-Mobile.js
@@ -23,7 +23,8 @@ Module.register('MMM-Mobile',{
         return {
             en: "translations/en.json",
             de: "translations/de.json",
-            es: "translations/es.json"
+            es: "translations/es.json",
+            es: "translations/id.json"
         };
     },
 
